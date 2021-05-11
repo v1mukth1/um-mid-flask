49931b31fb22b667e6ebf9a8cfa079c8eb3abefa
vimukthi_r <vimukthi.rajapaksha@my.sliit.lk>
2021-05-11 13:28:42 +0530

    with database tested, postman screenshots and postman collection

--- app.py
+++ app.py
@@ -1,51 +1,71 @@
 #~um-mid-flask/app.py
+#author Shakir
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from database.db import initialize_db
-from database.models import Book
+from database.models import Books
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-	'db' : 'your_database',
+	'db' : 'shakir',
     'host': 'localhost',
 	'port' : 27017
 }
 
 initialize_db(app)
 
+#this method returns a welcome note
 @app.route('/')
 def welcome():
     return {'welcome': 'um-mid-flask/app'}
 
+#this method returns all books
 @app.route('/books', methods=['GET'])
 def get_all_books():
-    book = Book.objects()
+    book = Books.objects()
     if not book:
+	    #book not found, return error
         return jsonify({'error': 'no books found!'})
     else:
         return jsonify(book.to_json())
 
+#this method returns a book searched by id
+@app.route('/books/<id>', methods=['GET'])
+def get_book_by_id(id):
+    book = Books.objects(id=id).first()
+    if not book:
+	    #book not found, return error
+        return jsonify({'error': 'invalid book id!'})
+    else:
+        return jsonify(book.to_json())
+
+#this method create a new book
 @app.route('/books', methods=['POST'])
 def add_book():
     body = request.get_json()
-    book = Book(**body).save()
+	#extracting book data from request
+    book = Books(**body).save()
     return jsonify(book.to_json())
 
+#this method update a book
 @app.route('/books/<id>', methods=['PUT'])
 def update_book(id):
     body = request.get_json()
-    book = Book.objects(id=body['id']).first()
+    book = Books.objects(id=body['id']).first()
     if not book:
+	    #book not found, return error
         return jsonify({'error': 'invalid book update request!'})
     else:
         book.update(**body)
-    return jsonify(book.to_json())
+        return jsonify(book.to_json())
 
+#this method delete a book
 @app.route('/books/<id>', methods=['DELETE'])
 def delete_book(id):
     body = request.get_json()
-    book = Book.objects(id=body['id']).first()
+    book = Books.objects(id=id).first()
     if not book:
+	    #book not found, return error
         return jsonify({'error': 'invalid book delete request!'})
     else:
         book.delete()
--- database/models.py
+++ database/models.py
@@ -1,12 +1,14 @@
 #~um-mid-flask/database/models.py
 from .db import db
 
-class Book(db.Document):
-    author = db.StringField()
-    country = db.StringField()
+class Books(db.Document):
+	__tablename__ = "books"
+	id = db.IntField(primary_key=True)
+	author = db.StringField()
+	country = db.StringField()
 	imageLink = db.StringField()
 	language = db.StringField()
 	link = db.StringField()
-	pages = db.IntegerField()
+	pages = db.IntField()
 	title = db.StringField()
-	year = db.IntegerField()
+	year = db.IntField()
